6d34f9becc3dc1f182aaa34a79ec97f269ebc019
Victor Barbosa <37561705+freitasdfb@users.noreply.github.com>
2022-01-29 01:44:54 -0300

    Background changing color according to time

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,23 @@ import Home from './components/home.components'
 function App() {
   return (
     <div className="App">
-      <div className='App-header'>
+      <div style={{ backgroundColor: changeBackground()}} className='App-header'>
         <Home />
       </div>
     </div>
   );
+
+
+  function changeBackground() {
+    const t = new Date().getHours();
+    if (t > 6 && t < 10) {
+     return '#6ad9ff'
+    } else if (t > 10 && t < 18) {
+      return "#ffc107";
+    } else {
+      return "#192048";
+    }
+  }
 }
 
 export default App;
